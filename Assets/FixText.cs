4173c6b15d202daabacfe5a7db12e62ca9b7c8e4
Mohammad Reza Taesiri <mtaesiri@gmail.com>
2019-05-04 12:35:18 +0430

    RTL Happened!

---
+++ Assets/FixText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class FixText : MonoBehaviour
+{
+    public string rtlText = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        var t = GetComponent<RTLTMPro.RTLTextMeshPro>();
+        t.text = rtlText;
+    }
+
+    private void OnValidate()
+    {
+        var t = GetComponent<RTLTMPro.RTLTextMeshPro>();
+        t.text = rtlText;
+    }
+}
